a7e51aa940454e5b80075ed3b66fa171452c3a15
Dung Ta Van <itprono3@gmail.com>
2020-12-16 23:10:26 +0700

    update performance test

--- src/main/java/com/tvd12/test/performance/Performance.java
+++ src/main/java/com/tvd12/test/performance/Performance.java
@@ -1,5 +1,9 @@
 package com.tvd12.test.performance;
 
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 /**
  * A builder class support for performance testing
  * This class run a script test and calculate 
@@ -16,6 +20,10 @@ public class Performance {
 	//number of script invocations
 	private int loopCount = 1000000;
 	
+	
+	//number of thread use to concurrent test mode
+	private int threadCount = 0;
+	
 	/**
 	 * builder method
 	 * 
@@ -23,20 +31,6 @@ public class Performance {
 	 */
 	public static Performance create() {
 		return new Performance();
-	}
-	
-	/**
-	 * execute script test and calculate offset time
-	 * 
-	 * @param script script to test
-	 * @return this pointer
-	 */
-	public Performance test(Script script) {
-		time = System.currentTimeMillis();
-		for(int i = 0 ; i < loopCount ; i++)
-			script.execute();
-		time = System.currentTimeMillis() - time;
-		return this;
 	}
 	
 	/**
@@ -51,10 +45,62 @@ public class Performance {
 	}
 	
 	/**
+	 * Set number of thread use in concurrent test mode
+	 * 
+	 * @param threadCount the number of threads
+	 * @return this pointer
+	 */
+	public Performance threadCount(int threadCount) {
+		this.threadCount = threadCount;
+		return this;
+	}
+	
+	/**
 	 * @return script running time
 	 */
 	public long getTime() {
 		return time;
+	}
+	
+	/**
+	 * execute script test and calculate offset time
+	 * 
+	 * @param script script to test
+	 * @return this pointer
+	 */
+	public Performance test(Script script) {
+		try {
+			if(threadCount <= 0)
+				testInSyncMode(script);
+			else
+				testInConcurrentMode(script);
+		}
+		catch (Exception e) {
+			throw new IllegalStateException(e);
+		}
+		return this;
+	}
+	
+	private void testInSyncMode(Script script) throws Exception {
+		long startTime = System.currentTimeMillis();
+		for(int i = 0 ; i < loopCount ; ++i)
+			script.execute();
+		time = System.currentTimeMillis() - startTime;
+	}
+	
+	private void testInConcurrentMode(Script script) throws Exception {
+		CountDownLatch barrier = new CountDownLatch(loopCount);
+		ExecutorService executorService = Executors.newFixedThreadPool(threadCount);
+		long startTime = System.currentTimeMillis();
+		for(int i = 0 ; i < loopCount ; ++i) {
+			executorService.execute(() -> {
+				script.execute();
+				barrier.countDown();
+			});
+		}
+		barrier.await();
+		time = System.currentTimeMillis() - startTime;
+		executorService.shutdown();
 	}
 	
 }
--- src/test/java/com/tvd12/test/testing/performance/PerformanceTest.java
+++ src/test/java/com/tvd12/test/testing/performance/PerformanceTest.java
@@ -2,10 +2,11 @@ package com.tvd12.test.testing.performance;
 
 import org.testng.annotations.Test;
 
+import com.tvd12.test.base.BaseTest;
 import com.tvd12.test.performance.Performance;
 import com.tvd12.test.performance.Script;
 
-public class PerformanceTest {
+public class PerformanceTest extends BaseTest {
 
     @Test
     public void test() {
@@ -17,6 +18,38 @@ public class PerformanceTest {
                     hello();
                 }
                 public void hello() {
+                }
+            })
+            .getTime();
+    }
+    
+    @Test
+    public void testInConcurrentMode() {
+        long time = Performance.create()
+            .loop(10000)
+            .threadCount(100)
+            .test(new Script() {
+                @Override
+                public void execute() {
+                    try {
+						Thread.sleep(1);
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					}
+                }
+            })
+            .getTime();
+        System.out.println(time);
+    }
+    
+    @Test(expectedExceptions = IllegalStateException.class)
+    public void testExceptionCase() {
+        Performance.create()
+            .loop(1000000000)
+            .test(new Script() {
+                @Override
+                public void execute() {
+                    throw new IllegalArgumentException("just test");
                 }
             })
             .getTime();
